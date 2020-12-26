c57270170fa94462c0d49f8f1d5b795e78b34c08
subhendra <subhendra995@gmail.com>
2020-12-26 20:57:11 +0530

    created a sprites

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@ const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
 
+
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
@@ -17,7 +18,7 @@ function setup(){
 
 
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+    platform = new Ground(150, 305, 350, 200);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
